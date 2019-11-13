5cc1e3c203df1cdc9d9a20ce2c37ea5ac3742f97
dkwitt <dominikawitt@gmail.com>
2019-11-13 03:12:53 +0100

    Refactored using lift up conditional (without Conjured Mana Cake)

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,90 +9,70 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-
-            /*
-            jeżeli to jest conjured mana cake to{coś}
-            w przeciwnym wypadku{reszta kodu}
-           1. Once the sell by date has passed, Quality degrades twice as fast
-2.The Quality of an item is never negative
-“Conjured” items degrade in Quality twice as fast as normal items
-             */
-            if (items[i].name.equals("Conjured Mana Cake")) {
-                if (items[i].sellIn > 0) {
-                    if (items[i].quality >=2) {
-                        items[i].quality -= 2;
-                    } else{
-                        if (items[i].quality == 1) {
-                            items[i].quality -= 1;
-                        }
-                    }
-
-                } else {
-                    if (items[i].quality >= 4) {
-                        items[i].quality -= 4;
-                    } else {
-                        if(items[i].quality == 3){
-                            items[i].quality -=3;
-                        }else if (items[i].quality == 2) {
-                            items[i].quality -=2;
-                        }else if (items[i].quality == 1) {
-                            items[i].quality -=1;
-                        }
-                    }
-                }
-                items[i].sellIn = items[i].sellIn - 1;
-            } else {
-
-
-                if (!items[i].name.equals("Aged Brie")
-                        && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (items[i].quality > 0) {
-                        if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                            items[i].quality = items[i].quality - 1;
-                        }
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-
-                        if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                            if (items[i].sellIn < 11) {
-                                if (items[i].quality < 50) {
-                                    items[i].quality = items[i].quality + 1;
-                                }
-                            }
-
-                            if (items[i].sellIn < 6) {
-                                if (items[i].quality < 50) {
-                                    items[i].quality = items[i].quality + 1;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                    items[i].sellIn = items[i].sellIn - 1;
-                }
-
-                if (items[i].sellIn < 0) {
-                    if (!items[i].name.equals("Aged Brie")) {
-                        if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                            if (items[i].quality > 0) {
-                                if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                    items[i].quality = items[i].quality - 1;
-                                }
-                            }
-                        } else {
-                            items[i].quality = items[i].quality - items[i].quality;
-                        }
-                    } else {
-                        if (items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1;
-                        }
-                    }
-                }
-            }
+            Item item = items[i];
+            doUpdateQuality(item);
         }
     }
-}
+
+    private void doUpdateQuality(Item item) {
+        switch (item.name) {
+            case "Aged Brie":
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+
+                }
+
+                item.sellIn = item.sellIn - 1;
+
+                if (item.sellIn < 0) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                }
+                break;
+            case "Backstage passes to a TAFKAL80ETC concert":
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+
+                    if (item.sellIn < 11) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
+                    }
+
+                    if (item.sellIn < 6) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
+                    }
+                }
+
+                item.sellIn = item.sellIn - 1;
+
+                if (item.sellIn < 0) {
+                    item.quality = item.quality - item.quality;
+                }
+                break;
+            case "Sulfuras, Hand of Ragnaros":
+
+                break;
+            default:
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1;
+                }
+
+                item.sellIn = item.sellIn - 1;
+
+                if (item.sellIn < 0) {
+                    if (item.quality > 0) {
+                        if (true) {
+                            item.quality = item.quality - 1;
+                        }
+                    }
+                }
+                break;
+        }
+
+        }
+    }
+
